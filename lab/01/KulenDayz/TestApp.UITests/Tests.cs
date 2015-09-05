a211ba810457172c612744ab66daad4df15b91b2
moljac Miljenko Cvjetko) <mcvjetko@holisticware.net>
2015-09-05 15:55:20 +0200

    lab from workshop/training

---
+++ lab/01/KulenDayz/TestApp.UITests/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace TestApp.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest ()
+		{
+			app = ConfigureApp.Android.StartApp ();
+		}
+
+		[Test]
+		public void ClickingButtonTwiceShouldChangeItsLabel ()
+		{
+
+			//app.Repl();
+
+			app.Tap(c=>c.Button("button2ndScreen"));;
+			app.Tap(c=>c.Button("button1"));;
+			app.Tap(c=>c.Button("button2ndScreen"));;
+			app.Tap(c=>c.Button("button1"));;
+			app.Tap(c=>c.Button("button2ndScreen"));;
+			app.Tap(c=>c.Button("button1"));;
+			app.Tap(c=>c.Button("button2ndScreen"));;
+			app.Tap(c=>c.Button("button1"));;
+
+//			Func<AppQuery, AppQuery> MyButton = c => c.Button ("button2ndScreen");
+//
+//			app.Tap (MyButton);
+//			app.Tap (MyButton);
+//			AppResult[] results = app.Query (MyButton);
+//			app.Screenshot ("Button clicked twice.");
+//
+//			Assert.AreEqual ("2 clicks!", results [0].Text);
+		}
+	}
+}
+
---
+++ lab/01/KulenDayz/TestApp/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TestApp
+{
+	[Activity (Label = "TestApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.button2ndScreen);
+			
+//			button.Click += delegate
+//			{
+//				button.Text = string.Format ("{0} clicks!", count++);
+//			};
+
+			// button.SetOnClickListener(new ClickListener());	
+
+			return;
+		}
+
+		class ClickListener : View.IOnClickListener
+		{
+			#region IOnClickListener implementation
+			public void OnClick (View v)
+			{
+				throw new NotImplementedException ();
+			}
+			#endregion
+			#region IDisposable implementation
+			public void Dispose ()
+			{
+				throw new NotImplementedException ();
+			}
+			#endregion
+			#region IJavaObject implementation
+			public IntPtr Handle
+			{
+				get
+				{
+					throw new NotImplementedException ();
+				}
+			}
+			#endregion
+		}
+
+		[Java.Interop.Export("sendMessage")]
+		public void sendMessage(View view) 
+		{
+			//Intent intent = new Intent(this, Screen2.class);
+			Intent intent = new Intent(this, typeof(Screen2Activity));
+			this.StartActivity(intent);
+		}
+
+	}
+}
+
+
